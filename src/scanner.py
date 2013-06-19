af1516efeb234359f35c3dd4e9f741666e968553
Anthony Burzillo <burz@jhu.edu>
2013-06-19 14:59:15 -0400

    Finished scanner

--- src/scanner.py
+++ src/scanner.py
@@ -34,25 +34,71 @@ class Scanner(object):
     if self.file_input:
       self.stream.close()
   def reset_partial(self):
-    self.integer_buffer = False
-    self.identifier_buffer = False
+    self.integer_partial = False
+    self.identifier_partial = False
     self.partial = ""
-  def new_token(self, token_type = False, set_partial = False):
+  def create_token(self, token_type = False, set_partial = False):
     if token_type:
       token = Token(self.line_number, token_type, self.partial)
     else:
       token = Token(self.line_number, self.partial)
-    self.integer_buffer = False
-    self.identifier_buffer = False
+    self.integer_partial = False
+    self.identifier_partial = False
     if set_partial:
       if 'a' <= set_partial <= 'z' or 'A' <= set_partial <= 'Z':
-        self.identifier_buffer = True
+        self.identifier_partial = True
       elif '0' <= set_partial <= '9':
-        self.integer_buffer = True
+        self.integer_partial = True
       self.partial = set_partial
     else:
       self.partial = ""
     return token
+  def partial_to_token(self, c = False):
+    if self.partial in [';', '.', '=', ')', '+', '-', '*', '#', '[', ']', ',']:
+      return self.create_token(set_partial = c)
+    elif self.partial in [':', '<', '>']:
+      if c == "=":
+        self.partial += c
+        return self.create_token()
+      return self.create_token(set_partial = c)
+    elif self.partial == '(':
+      if c == '*':
+        line_number = self.line_number
+        last = False
+        while True:
+          c = self.stream.read(1)
+          if not c:
+            raise Scanner_error("The comment on line {} is not closed".format(line_number))
+          elif c == '\n':
+            self.line_number += 1
+          elif c == '*':
+            last = True
+          elif last and c == ')':
+            break
+        self.reset_partial()
+      else:
+        return self.create_token(set_partial = c)
+    elif self.integer_partial:
+      if '0' <= c <= '9':
+        self.partial += c
+        return False
+      elif 'a' <= c <= 'z' or 'A' <= c <= 'Z':
+        raise Scanner_error("The identifier '{}' on line {} starts with a number".format(
+                              self.partial, self.line_number))
+      else:
+        return self.create_token('integer', c)
+    elif self.identifier_partial:
+      if 'a' <= c <= 'z' or 'A' <= c <= 'Z' or '0' <= c <= '9':
+        self.partial += c
+        return False
+      elif self.partial in ['PROGRAM', 'BEGIN', 'END', 'CONST', 'TYPE', 'VAR', 'PROCEDURE',
+                            'RETURN', 'ARRAY', 'OF', 'RECORD', 'DIV', 'MOD', 'IF', 'THEN',
+                            'ELSE', 'REPEAT', 'UNTIL', 'WHILE', 'DO', 'WRITE', 'READ', 'INTEGER']:
+        return self.create_token(set_partial = c)
+      else:
+        return self.create_token('identifier', c)
+    else:
+      raise Scanner_error("Urecognized symbol '{}' on line {}".format(self.partial, self.line_number))
   def generate_tokens(self):
     tokens = []
     while True:
@@ -67,77 +113,23 @@ class Scanner(object):
       if not c:
         self.eof = True
         if self.partial:
-          self.line_number -= 1
-          if self.integer_buffer:
-            return self.new_token('integer')
-          elif self.identifier_buffer:
-            return self.new_token('identifier')
-          elif self.partial in [';', '.', '=', ')', '+', '-', '*', '#', '[',
-                                ']', ',', ':', '<', '>']:
-            return self.new_token()
-          else:
-            raise Scanner_error("Urecognized symbol '{}' on line {}".format(
-                                  self.partial, self.line_number))
+          return self.partial_to_token()
         return False
       if c in [' ', '\f', '\r', '\t', '\n']:
         if c == '\n':
           self.line_number += 1
         if self.partial:
-          if self.integer_buffer:
-            return self.new_token('identifier')
-          if self.identifier_buffer:
-            return self.new_token('integer')
+          return self.partial_to_token()
         continue
       if len(self.partial) >= 1:
-        if self.partial in [';', '.', '=', ')', '+', '-', '*', '#', '[', ']', ',']:
-          return self.new_token(set_partial = c)
-        if self.partial in [':', '<', '>']:
-          if c == "=":
-            self.partial += c
-            return self.new_token()
-          return self.new_token(set_partial = c)
-        if self.partial == '(':
-          if c == '*':
-            line_number = self.line_number
-            last = False
-            while True:
-              c = self.stream.read(1)
-              if not c:
-                raise Scanner_error("The comment on line {} is not closed".format(line_number))
-              elif c == '\n':
-                self.line_number += 1
-              elif c == '*':
-                last = True
-              elif last and c == ')':
-                break
-            self.reset_partial()
-            continue
-          else:
-            return self.new_token(set_partial = c)
-        if self.partial in ['PROGRAM', 'BEGIN', 'END', 'CONST', 'TYPE', 'VAR', 'PROCEDURE',
-                            'RETURN', 'ARRAY', 'OF', 'RECORD', 'DIV', 'MOD', 'IF',
-                            'THEN', 'ELSE', 'REPEAT', 'UNTIL', 'WHILE', 'DO', 'WRITE',
-                            'READ']:
-          return self.new_token(set_partial = c)
-        if self.integer_buffer:
-          if '0' <= c <= '9':
-            self.partial += c
-            continue
-          elif 'a' <= c <= 'z' or 'A' <= c <= 'Z':
-            raise Scanner_error("The identifier '{}' on line {} starts with a number".format(
-                                  self.partial, self.line_number))
-          return self.new_token('integer', c)
-        if self.identifier_buffer:
-          if 'a' <= c <= 'z' or 'A' <= c <= 'Z' or '0' <= c <= '9':
-            self.partial += c
-            continue
-          return self.new_token('identifier', c)
-        raise Scanner_error("Urecognized symbol '{}' on line {}".format(
-                              self.partial, self.line_number))
+        token = self.partial_to_token(c)
+        if not token:
+          continue
+        return token
       if '0' <= c <= '9':
-        self.integer_buffer = True
+        self.integer_partial = True
       elif 'a' <= c <= 'z' or 'A' <= c <= 'Z':
-        self.identifier_buffer = True
+        self.identifier_partial = True
       self.partial += c
     return False
 
