6d834d22cf23cd6d94a630c55de07f0404c43096
Anthony Burzillo <burz@jhu.edu>
2013-06-24 00:39:36 -0400

    Finished graphical for syntax tree

--- src/syntax_tree.py
+++ src/syntax_tree.py
@@ -81,23 +81,39 @@ class Read(Node):
   def __init__(self, location, line):
     self.location = location
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Read",shape=rectangle]'
+    location = self.location.graphical()
+    print node, '->', location, '[label="location"]'
+    return node
 
 class Write(Node):
   def __init__(self, expression, line):
     self.expression = expression
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Write",shape=rectangle]'
+    expression = self.expression.graphical()
+    print node, '->', expression, '[label="expression"]'
+    return node
 
 class Location(Node):
   def __init__(self, child, type_object, line):
     self.child = child
     self.type_object = type_object
     self.line = line
+  def graphical(self):
+    return self.child.graphical()
 
 class Expression(Node):
   def __init__(self, child, type_object, line):
     self.child = child
     self.type_object = type_object
     self.line = line
+  def graphical(self):
+    return self.child.graphical()
 
 class Condition(Node):
   def __init__(self, relation, expression_left, expression_right, line):
@@ -105,12 +121,27 @@ class Condition(Node):
     self.expression_left = expression_left
     self.expression_right = expression_right
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="' + self.operator + '",shape=rectangle]'
+    left = self.expression_left.graphical()
+    print node, '->', left, '[label="left"]'
+    right = self.expression_right.graphical()
+    print node, '->', right, '[label="right"]'
+    return node
 
 class Variable(Node):
   def __init__(self, name, table_entry, line):
     self.name = name
     self.table_entry = table_entry
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Variable",shape=rectangle]'
+    variable = self.new_node()
+    print variable, '[label="' + self.name + '",shape=circle]'
+    print node, '->', variable, '[label="ST"]'
+    return node
 
 class Index(Node):
   def __init__(self, location, expression, type_object, line):
@@ -118,6 +149,14 @@ class Index(Node):
     self.expression = expression
     self.table_entry = type_object
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Index",shape=rectangle]'
+    location = self.location.graphical()
+    print node, '->', location, '[label="location"]'
+    expression = self.expression.graphical()
+    print node, '->', expression, '[label="expression"]'
+    return node
 
 class Field(Node):
   def __init__(self, location, variable, type_object, line):
@@ -125,11 +164,26 @@ class Field(Node):
     self.variable = variable
     self.table_entry = type_object
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Field",shape=rectangle]'
+    location = self.location.graphical()
+    print node, '->', location, '[label="location"]'
+    variable = self.variable.graphical()
+    print node, '->', variable, '[label="variable"]'
+    return node
 
 class Number(Node):
   def __init__(self, table_entry, line):
     self.table_entry = table_entry
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Number",shape=rectangle]'
+    value = self.new_node()
+    print value, "[label=\"{}\",shape=diamond]".format(self.table_entry.value)
+    print node, '->', value, '[label="ST"]'
+    return node
 
 class Binary(Node):
   def __init__(self, operator, expression_left, expression_right, line):
@@ -137,13 +191,38 @@ class Binary(Node):
     self.expression_left = expression_left
     self.expression_right = expression_right
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="' + self.operator + '",shape=rectangle]'
+    left = self.expression_left.graphical()
+    print node, '->', left, '[label="left"]'
+    right = self.expression_right.graphical()
+    print node, '->', right, '[label="right"]'
+    return node
 
 class Call(Node):
-  def __init__(self, definition, actual_expressions, type_object, line):
+  def __init__(self, definition, actuals, type_object, line):
     self.definition = definition
-    self.actual_expressions = actual_expressions
+    self.actuals = actuals
     self.table_entry = type_object
     self.line = line
+  def graphical(self):
+    node = self.new_node()
+    print node, '[label="Call",shape=rectangle]'
+    procedure = self.new_node()
+    print procedure, '[label="' + self.definition.name + '"]'
+    print node, '->', procedure, '[label="procedure"]'
+    if self.actuals:
+      cluster = self.new_cluster()
+      print 'subgraph', cluster, '{'
+      new_nodes = []
+      for actual in actuals:
+        expression = actual.graphical()
+        new_nodes.append(expression)
+      print '}'
+      for i, formal in enumerate(self.definition.formals):
+        print node, '->', new_nodes[i], '[label="' + formal + '"]'
+      return node
 
 class Syntax_tree(object):
   def __init__(self, instructions):
